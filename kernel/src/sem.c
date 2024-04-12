1a981dbd4294ddafe542b05b2d06df0139c8b069
Kurfuerst <bai3641@outlook.com>
2024-04-12 22:03:28 +0800

    task7-codeTask1

--- kernel/src/sem.c
+++ kernel/src/sem.c
@@ -9,12 +9,21 @@ void sem_init(sem_t *sem, int value) {
 
 void sem_p(sem_t *sem) {
   // Lab2-4: dec sem's value, if value<0, add curr proc to waitlist and block it
-  TODO();
+  // TODO();
+  sem->value--;
+  if (sem->value < 0) {
+    list_enqueue(&sem->wait_list, proc_curr());
+    proc_block();
+  }
 }
 
 void sem_v(sem_t *sem) {
   // Lab2-4: inc sem's value, if value<=0, dequeue a proc from waitlist and ready it
-  TODO();
+  // TODO();
+  sem->value++;
+  if (sem <= 0) {
+    proc_addready(list_dequeue(&sem->wait_list));
+  }
 }
 
 #define USER_SEM_NUM 128
